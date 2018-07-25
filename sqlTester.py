72cc9d4b503e105951d3606503d05085645867c9
Aeul <41166372+Aeul@users.noreply.github.com>
2018-07-24 21:11:38 -0500

    update

--- sqlTester.py
+++ sqlTester.py
@@ -6,7 +6,7 @@ import os
 import sys
 import shutil
 
-DRIVER = 'Driver={ODBC Driver 13 for SQL Server};Server=tcp:aeul-test-server.database.windows.net,1433;Database=Aaron_Test_Server;Uid=aeul@aeul-test-server;Pwd={/Raindrop18};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+DRIVER = 'Driver={ODBC Driver 17 for SQL Server};Server=tcp:aeul-test-server.database.windows.net,1433;Database=Aaron_Test_Server;Uid=aeul@aeul-test-server;Pwd={/Raindrop18};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
 
 PROCESSES = 5
 
